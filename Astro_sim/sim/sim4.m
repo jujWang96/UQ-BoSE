5094b69fd81eccaa18bf950340e1ae54aec49424
jujWang96 <jujwang@ucdavis.edu>
2025-04-02 20:37:28 -0700

    remove catch

--- Astro_sim/sim/sim4.m
+++ Astro_sim/sim/sim4.m
@@ -12,7 +12,6 @@ for i = 1:length(factors)
         %disp(['factor=', num2str(factor), ',sample_factor=', num2str(sample_factor), ',seed=', num2str(seed)])
         % generate simulated data (inhomogeneous Poisson point process)
         X = sim_inhomo_Pois_const([0 1], [0 1], sample_factor * lambda, loc, radius, sample_factor * factor * base_num_in_circle, seed);
-        try
             [labeled_cells, pred_class_all, n_region(index),merge_time(index)] = sim_fit_fast(X, factor, sample_factor, seed, false,gridszie,seedsize);
             true_class_all = evaluate_points_sim4(X, loc, radius);
             % only care about labeled cells
@@ -21,8 +20,6 @@ for i = 1:length(factors)
     
             % it is symmetric
             metrics(index) = rand_index(true_class_all, pred_class_all, 'adjusted');
-        catch ME
-        end
     end
 end
 
